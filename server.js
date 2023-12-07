d1c542abb43764f741848e6086e53262e1d351e2
Maxime Lacasse Germain <mlacassegermain@cmaisonneuve.qc.ca>
2023-12-06 22:01:33 -0500

    Ajout d'une classe de log

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const http = require("http");
+const Logger = require("./classes/Logger.js");
+
+//Initialisation d'un fichier de variables d'environnement
 require("dotenv").config();
 
+//Ajout d'un fichier de log
+const logger = new Logger();
+
+//Création d'un serveur
 const server = http.createServer((request, response) => {
     response.writeHead(200);
     response.end("<h1>Hello</h1>");
@@ -9,6 +16,7 @@ const server = http.createServer((request, response) => {
 //On assigne un port par défaut si aucune variable d'environnement n'est définie
 const port = process.env.port || 5000;
 
+// Démarrage du serveur
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    logger.info(`Server running on port ${port}`);
 });
