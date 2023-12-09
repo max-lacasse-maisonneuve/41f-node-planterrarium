8fa04c8b5b2129ba2873b42ce577ce89290fb0d8
Maxime Lacasse Germain <mlacassegermain@cmaisonneuve.qc.ca>
2023-12-09 08:19:39 -0500

    Cours 2 - ajout d'une requete et d'un moteur de gabarit

---
+++ data/donneesTest.js
@@ -0,0 +1,121 @@
+const donneesTest = [
+    {
+        id: 1,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 2,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 3,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 4,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 5,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 6,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 7,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 8,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 9,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 10,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 11,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 12,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 13,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 14,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 15,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 16,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+
+    {
+        id: 17,
+        nom: "Test",
+        prenom: "Test",
+        email: "",
+    },
+];
+module.exports = donneesTest;
--- server.js
+++ server.js
@@ -1,22 +1,64 @@
 const http = require("http");
 const Logger = require("./classes/Logger.js");
+const express = require("express");
+const path = require("path");
+const mustacheExpress = require("mustache-express");
+const { log } = require("console");
+const app = express();
 
 //Initialisation d'un fichier de variables d'environnement
 require("dotenv").config();
-
 //Ajout d'un fichier de log
 const logger = new Logger();
-
-//Création d'un serveur
-const server = http.createServer((request, response) => {
-    response.writeHead(200);
-    response.end("<h1>Hello</h1>");
-});
-
 //On assigne un port par défaut si aucune variable d'environnement n'est définie
 const port = process.env.port || 5000;
 
+// Configurateur d'un moteur de gabarit
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "mustache");
+app.engine("mustache", mustacheExpress());
+
+// Middlewares
+// Ajoute un dossier public pour les fichiers statiques (css, js, images). Ce dossier est accessible depuis le navigateur. Cela permet de ne pas avoir à créer de route pour chaque fichier statique.
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+// Toutes les routes non statiques doivent être définies après les middlewares
+app.get("/donnees", (req, res) => {
+    const donneesTest = require("./data/donneesTest.js");
+    logger.info(donneesTest);
+
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.json(donneesTest);
+});
+
+app.get("/donnees/:id", (req, res) => {
+    logger.info(req.params.id);
+    const donneesTest = require("./data/donneesTest.js");
+    const donnees = donneesTest.find((element) => element.id == req.params.id);
+
+    if (donnees) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 200;
+        res.json(donnees);
+    } else {
+        res.setHeader("Content-Type", "text/html");
+        res.statusCode = 404;
+        res.json({ message: "Donnée non trouvée" });
+    }
+});
+
+// Page 404
+// Cette route doit être définie en dernier pour que les autres routes soient testées en premier
+// Si aucune route n'est trouvée, alors on affiche la page 404
+app.use((req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.statusCode = 404;
+    res.render("404", { url: req.url });
+});
+
 // Démarrage du serveur
-server.listen(port, () => {
+app.listen(port, () => {
     logger.info(`Server running on port ${port}`);
 });
